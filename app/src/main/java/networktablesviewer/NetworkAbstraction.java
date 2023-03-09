c87d725c9e94119171ec3b590bc2b81b8bf01aa2
mebrahimaleem <mebrahimaleem@gmail.com>
2023-03-08 21:45:52 -0800

    Added MultiSubscriber, untested

--- app/src/main/java/networktablesviewer/NetworkAbstraction.java
+++ app/src/main/java/networktablesviewer/NetworkAbstraction.java
@@ -1,15 +1,23 @@
 package networktablesviewer;
 
 import edu.wpi.first.networktables.*;
-import java.util.ArrayList;
+import java.util.*;
 
-public class NetworkAbstraction{
+public class NetworkAbstraction {
 	NetworkTableInstance netinst;
-	private int internalState;
+	int internalState; //TODO: use internalState to track errors
+	MultiSubscriber subs;
+	NetworkTableListenerPoller poll;
 
 	public NetworkAbstraction(){
 		netinst = NetworkTableInstance.create();
 		netinst.startClient4("NTVC-lcl");
+
+		subs = new MultiSubscriber(netinst, new String[] {"/"});
+		poll = new NetworkTableListenerPoller(netinst);
+		poll.addListener(subs, EnumSet.of(NetworkTableEvent.Kind.kValueAll));
+
+		internalState = 0;
 	}
 
 	public void connect(){
@@ -24,7 +32,35 @@ public class NetworkAbstraction{
 		return netinst.isConnected();
 	}
 
-	public void close(){
+	public void close() {
+		poll.close();
+		subs.close();
 		netinst.close();
+	}
+
+	public int getError(){
+		return internalState;
+	}
+
+	public ArrayList<NetworkTableValue> getLatest(){
+		ArrayList<NetworkTableValue> values = new ArrayList<NetworkTableValue>();
+		NetworkTableEvent[] events = poll.readQueue();
+
+		for (NetworkTableEvent event : events){
+			values.add(event.valueData.value);
+		}
+
+		return values;
+	}
+
+	public static ArrayList<NetworkTableValue> squashLatest(ArrayList<NetworkTableValue> current, ArrayList<NetworkTableValue> latest){
+		for (int i = 0; i < current.size(); i++)
+			for (int j = 0; j < latest.size(); j++)
+				if (current.get(i).getTime() < latest.get(j).getTime()){
+					current.remove(i);
+					current.add(i, latest.get(j));
+					latest.remove(j);
+				}
+		return current;
 	}
 }
