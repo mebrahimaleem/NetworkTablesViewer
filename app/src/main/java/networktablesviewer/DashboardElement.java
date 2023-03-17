fa4331af0084a73433deebc3ae8486e839d18085
mebrahimaleem <mebrahimaleem@gmail.com>
2023-03-16 20:44:14 -0700

    Implemented moving DashboardElements

--- app/src/main/java/networktablesviewer/DashboardElement.java
+++ app/src/main/java/networktablesviewer/DashboardElement.java
@@ -12,6 +12,10 @@ public class DashboardElement extends JPanel {
 	JLabel title;
 	JLabel value;
 	boolean dragging = false;
+	boolean moving = false;
+	int ymoveOf = 0;
+	int xmoveOf = 0;
+	Point moveTarget = new Point();
 	Point dragOrigin = new Point();
 	final int borderSz = 12;
 
@@ -40,11 +44,20 @@ public class DashboardElement extends JPanel {
 					dragging = true;
 					setCursor(new Cursor(Cursor.SE_RESIZE_CURSOR));
 				}
+				else if (dragOrigin.getY() < getY() + borderSz){
+					moving = true;
+					setCursor(new Cursor(Cursor.MOVE_CURSOR));
+					ymoveOf = (int)dragOrigin.getY() - getY();
+					xmoveOf = (int)dragOrigin.getX() - getX();
+					moveTarget = getLocation();
+				}
 			}
 
 			@Override
 			public void mouseReleased(MouseEvent e){
 				dragging = false;
+				if (moving) setLocation(moveTarget);
+				moving = false;
 				setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
 				revalidate();
 				repaint();
@@ -54,12 +67,17 @@ public class DashboardElement extends JPanel {
 		this.addMouseMotionListener(new MouseMotionAdapter() {
 			@Override
 			public void mouseDragged(MouseEvent e){
-				if (!dragging) return;
+				if (dragging) {
+					setSize((int)(getWidth() + (e.getPoint().getX() - dragOrigin.getX())), (int)(getHeight() + (e.getPoint().getY() - dragOrigin.getY())));
+					if (getSize().width < 2 * borderSz || getSize().height < 2 * borderSz) 
+						setSize(Math.max(2 * borderSz, getSize().width), Math.max(2 * borderSz, getSize().height));
+					dragOrigin = e.getPoint();
+				}
 
-				setSize((int)(getWidth() + (e.getPoint().getX() - dragOrigin.getX())), (int)(getHeight() + (e.getPoint().getY() - dragOrigin.getY())));
-				dragOrigin = e.getPoint();
+				else if (moving){
+					moveTarget = new Point((int)e.getPoint().getX() - xmoveOf, (int)e.getPoint().getY() - ymoveOf);
+				}
 			}
-
 		});
 	}
 }
